6be4d375f0e308d03f03905f316db2b51134c8f4
Amr abdul-aty el-hafy <58088485+amrel7afy@users.noreply.github.com>
2024-07-08 13:53:53 +0300

    edit collapse to show visit price.

--- lib/features/select_your_plan/presentation/view/widgets/expansion_list_view.dart
+++ lib/features/select_your_plan/presentation/view/widgets/expansion_list_view.dart
@@ -1,16 +1,7 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:test1/core/AppRouter.dart';
 import 'package:test1/core/constants/constants.dart';
-import 'package:test1/core/constants/vertical_and_horizontal_space.dart';
-import 'package:test1/core/helper/extensions.dart';
-import 'package:test1/core/theming/my_colors.dart';
-import 'package:test1/core/theming/styles.dart';
-import 'package:test1/features/select_your_plan/presentation/view/widgets/plan_duration_list_view.dart';
 import 'collapse_card.dart';
-import 'my_choice_chip.dart';
-import 'choice_chips_list_view.dart';
+
 class ExpansionListView extends StatelessWidget {
   const ExpansionListView({
     super.key,
@@ -18,14 +9,14 @@ class ExpansionListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(delegate: SliverChildBuilderDelegate(
-            (context, index) {
-          return const Padding(
-            padding: EdgeInsets.only(bottom: 17.0),
-            child: CollapseCard(),
-          );
-        },
-        childCount: 4
-    ),);
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(AppConstants.k21ViewPadding, 0,
+              AppConstants.k21ViewPadding, 17.0),
+          child: const CollapseCard(showVisitPrice: false,),
+        );
+      }, childCount: 4),
+    );
   }
 }
