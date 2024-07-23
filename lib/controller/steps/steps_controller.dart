236bbb99b4d91f8bd612fdd80015b804fa22c50e
Amr abdul-aty el-hafy <58088485+amrel7afy@users.noreply.github.com>
2024-07-23 17:11:06 +0300

    - fix query params of first step✅.

--- lib/controller/steps/steps_controller.dart
+++ lib/controller/steps/steps_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:test1/cubit/loader_cubit/loader_cubit.dart';
+import 'package:test1/models/fist_step/first_step_obj_parameter.dart';
 import 'package:test1/models/steps/step_details_vm.dart';
 import 'package:test1/services/app_service.dart';
 import 'package:test1/utility/repository/repository.dart';
@@ -12,9 +13,14 @@ class StepsController {
 
 
   static Future<StepDetailsVm?> fetchFirstStep() async {
+    final String object=jsonEncode(FirstStepObjParameter(
+        fromOffer: false,
+        selectedPricingId: null,
+        serviceId: Repository.supServiceId
+    ).toJson());
     var result = await AppService.callService(
         actionType: ActionType.get,
-        apiName: '/api/Steps/FirstStep?serviceType=2&Object=%7B%22ServiceId%22:%22c97fdb23-4687-ec11-a837-000d3abe20f8%22,%22SelectedPricingId%22:null,%22FromOffer%22:false%7D',
+        apiName: '/api/Steps/FirstStep?serviceType=2&Object=$object',
         body: null);
     StepDetailsVm step = StepDetailsVm.fromJson(jsonDecode(result));
     return result != null ? step : null;
